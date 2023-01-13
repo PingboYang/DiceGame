de7949f787ebdf6b80b3dd76968e75aca0eefbf7
Pingbo Yang <86988793+PingboYang@users.noreply.github.com>
2023-01-12 23:14:03 -0500

    Added files

---
+++ dice/src/images.js
@@ -0,0 +1,13 @@
+export default{
+    "data":{
+        "diceImage": [
+        {"url": "https://upload.wikimedia.org/wikipedia/commons/c/c5/Dice-1.png"},
+        {"url": "https://upload.wikimedia.org/wikipedia/commons/1/18/Dice-2.png"},
+        {"url": "https://upload.wikimedia.org/wikipedia/commons/7/70/Dice-3.png"},
+        {"url": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Dice-4.svg/557px-Dice-4.svg.png"},
+        {"url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Dice-5.svg/1200px-Dice-5.svg.png"},
+        {"url": "https://upload.wikimedia.org/wikipedia/commons/5/5c/Dice-6.png"}
+    ]
+    }
+    
+}
